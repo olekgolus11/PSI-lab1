5dc0f5f80c7eb1107dc086f5bbb790986fd2a5df
Olek Golus <olekgolus02@gmail.com>
2023-10-10 23:44:06 +0200

    Add StudentAI class, add predict and layer adding

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 
 class StudentAI:
     weights_matrix_list = None
-    number_of_entry_values = None
 
     def __init__(self, number_of_entry_values):
         self.number_of_entry_values = number_of_entry_values
@@ -21,9 +20,23 @@ class StudentAI:
         return inputs
 
     def add_layer(self, n, weight_range_values):
+        min_value = weight_range_values[0]
+        max_value = weight_range_values[1]
 
-    def generate_weight_matrix(self, n, weight_range_values):
+        if self.weights_matrix_list is None:
+            entry_values_count = self.number_of_entry_values
+            matrix_layer = np.matrix(np.random.uniform(min_value, max_value, (n, entry_values_count)))
+            self.weights_matrix_list = [matrix_layer]
+        else:
+            entry_values_count = self.weights_matrix_list[-1].shape[0]
+            matrix_layer = np.matrix(np.random.uniform(min_value, max_value, (n, entry_values_count)))
+            self.weights_matrix_list.append(matrix_layer)
 
+    def predict(self, input_values):
+        return self.deep_neural_network(input_values, self.weights_matrix_list)
+
+    def load_weights(self, file_name):
+        pass # TODO
 
 # ZAD 2
 weights_matrix = np.matrix('0.1 0.1 -0.3; 0.1 0.2 0.0; 0.0 0.7 0.1; 0.2 0.4 0.0; -0.3 0.5 0.1')
@@ -34,6 +47,12 @@ input_vector = np.matrix('0.5; 0.75; 0.1')
 weights_matrix_1 = weights_matrix
 weights_matrix_2 = np.matrix('0.7 0.9 -0.4 0.8 0.1; 0.8 0.5 0.3 0.1 0.0; -0.3 0.9 0.3 0.1 -0.2')
 weights_matrix_list = np.array([weights_matrix_1, weights_matrix_2], 'object')
-print(deep_neural_network(input_vector, weights_matrix_list))
 
 # ZAD 4
+studentAI = StudentAI(3)
+studentAI.add_layer(5, (-0.5, 0.5))
+studentAI.add_layer(4, (-0.5, 0.5))
+studentAI.add_layer(2, (-0.5, 0.5))
+studentAI.add_layer(1, (-0.5, 0.5))
+input_values = np.matrix('1; 5; 3')
+print(studentAI.predict(input_values))
